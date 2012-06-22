bba105227b90c282238637a59203ef1a7518967e
fkubo <kubo.fumihiro@ns-sol.co.jp>
2012-06-22 20:57:27 +0900

    Update gh-pages

--- core/src/main/java/org/junithelper/core/meta/TestMethodMeta.java
+++ core/src/main/java/org/junithelper/core/meta/TestMethodMeta.java
@@ -1,17 +1,17 @@
-/* 
- * Copyright 2009-2010 junithelper.org. 
+/*
+ * Copyright 2009-2010 junithelper.org.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- *     http://www.apache.org/licenses/LICENSE-2.0 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- * either express or implied. See the License for the specific language 
- * governing permissions and limitations under the License. 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 package org.junithelper.core.meta;
 
@@ -23,6 +23,10 @@ public class TestMethodMeta {
     public boolean isTypeTest;
 
     public boolean isInstantiationTest;
+
+    // fk 2012.06.20 複数コンストラクタ対応.
+    public ConstructorMeta constructorMeta;
+    // fk
 
     public ClassMeta classMeta;
 
